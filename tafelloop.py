b29c10afc37285a5bd6e0d927fc6c0975bfeb90a
Gavin <99065061@mydavinci.nl>
2022-01-21 21:33:21 +0100

    1

---
+++ tafelloop.py
@@ -0,0 +1,42 @@
+# Maak daarin 4 programma's met for loops:
+# Print de tafel van een op te geven getal met input()  Bijv. 1 x 4 = 4 ; 2 x 4 = 8 etc t/m 10 x 4 = 40
+# Vanaf 30 terugtellen tot de raketlancering. Print elke tel en print de lancering.
+# Print de hele uren van de dag. Voor de ochtend voeg je 'AM' toe. Voor de middag voeg je 'PM' toe
+# Print alle even getallen tussen 20 en 50
+# Doe een commit
+# Lever hier de zip in van de map tafelmanieren
+
+
+
+
+tafel = input("Welke tafel wilt u uitrekenen? ")
+if tafel == "1":   
+    for i in range(1,11,):
+        print(i * 1)
+elif tafel == "2":   
+    for i in range(1,11,):
+        print(i * 2)
+elif tafel == "3":   
+    for i in range(1,11,):
+        print(i * 3)
+elif tafel == "4":   
+    for i in range(1,11,):
+        print(i * 4)
+elif tafel == "5":   
+    for i in range(1,11,):
+        print(i * 5)
+elif tafel == "6":   
+    for i in range(1,11,):
+        print(i * 6)
+elif tafel == "7":   
+    for i in range(1,11,):
+        print(i * 7)
+elif tafel == "8":   
+    for i in range(1,11,):
+        print(i * 8)
+elif tafel == "9":   
+    for i in range(1,11,):
+        print(i * 9)
+elif tafel == "10":   
+    for i in range(1,11,):
+        print(i * 10)
